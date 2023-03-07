1b8cb95a74adb1c73d1cb9130238535d7b362061
程学明 <1475255466@qq.com>
2023-03-07 21:19:14 +0800

    初始化手脚架项目

---
+++ bin/index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+const program = require('commander')
+const packageJson = require("../package.json")
+
+
+program
+  .version(packageJson.version)
+  .command("create <app-name>")
+  .description("create a new project")
+  .option("--f, --force", "overwrite target directory if it exist") // 是否强制创建，当文件夹已经存在
+  .action((name, options) => {
+    console.log(name, options);
+  })
+// 拿到命令行参数
+program.parse(process.argv)
